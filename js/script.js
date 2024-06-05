f8a998fc5d6ed66782e078d17f7dc874f015bcf2
Christian Noel Salvador <christiannoelsalvador24@gmail.com>
2024-06-05 11:53:18 +0800

    navbar changes

--- js/script.js
+++ js/script.js
@@ -12,16 +12,47 @@ document.addEventListener('DOMContentLoaded', function () {
   const submenu = document.getElementsByClassName('submenu');
   const menuLink = document.getElementsByClassName('menu-link');
 
-
-    // for (let i = 0; i <= submenu.length; i++) {
-    //     submenu[i].addEventListener('transitionend', function(e) {
-    //         submenu[i].style.visibility = 'hidden'
-    //     });
-        
-    //     menuLink[i].addEventListener('mouseover', function(e) {
-    //         submenu[i].style.visibility = 'visible'
-    //     })
-    // }
+  for (let i = 0; i < submenu.length; i++) {
+    let isSubmenuHovered = false;
+  
+    menuLink[i].addEventListener('mouseenter', function(e) {
+      submenu[i].classList.add('jelly-animate');
+      submenu[i].classList.remove('fade-down');
+      submenu[i].style.opacity = '1';
+      submenu[i].style.display = 'block';
+    });
+  
+    submenu[i].addEventListener('mouseenter', function(e) {
+      isSubmenuHovered = true;
+      submenu[i].classList.add('jelly-animate');
+      submenu[i].classList.remove('fade-down');
+      submenu[i].style.opacity = '1';
+      submenu[i].style.display = 'block';
+    });
+  
+    submenu[i].addEventListener('mouseleave', function(e) {
+      isSubmenuHovered = false;
+      setTimeout(function() {
+        if (!isSubmenuHovered) {
+          submenu[i].classList.remove('jelly-animate');
+          submenu[i].classList.add('fade-down');
+          submenu[i].style.display = 'none';
+          submenu[i].style.opacity = '0';
+        }
+      }, 200);
+    });
+  
+    menuLink[i].addEventListener('mouseleave', function(e) {
+      setTimeout(function() {
+        if (!isSubmenuHovered) {
+          submenu[i].classList.remove('jelly-animate');
+          submenu[i].classList.add('fade-down');
+          submenu[i].style.display = 'none';
+          submenu[i].style.opacity = '0';
+        }
+      }, 200);
+    });
+  }
 
   const checkboxes = document.querySelectorAll('.group-checkbox');
 
